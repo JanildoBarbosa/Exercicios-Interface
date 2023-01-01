338964eb108bad2401805dc0d8720dfd30d67b33
Janildo Barbosa <janildofbabosa@gmail.com>
2022-12-31 23:53:01 -0300

    Created classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do aluguel ");
+		System.out.print("Modelo do carro: ");
+		String carModel = scanner.nextLine();
+		System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(scanner.nextLine(), dtf);
+		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(scanner.nextLine(), dtf);
+		
+		CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+		
 
+		scanner.close();
 	}
 
 }
